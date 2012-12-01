f6c43be9eaa817b31ca1c3944704228d07f9112c
Igor Vuk <parcijala@gmail.com>
2012-12-01 13:53:19 +0100

    FizzBuzz in Python

---
+++ fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# vim: ts=2
+
+for a in range(1,101):
+
+	if a % 15 == 0:
+		print 'FizzBuzz'
+	elif a % 5 == 0:
+		print 'Buzz'
+	elif a % 3 == 0:
+		print 'Fizz'
+	else:
+		print a
